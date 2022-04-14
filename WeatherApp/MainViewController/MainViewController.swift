8f7c523d79c994eabb82d131b22b272c4c90a2f4
Defalt Lee <genesis20150601@gmail.com>
2022-04-14 14:32:31 +0800

    First commit

---
+++ WeatherApp/MainViewController/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  WeatherApp
+//
+//  Created by Defalt Lee on 2022/4/14.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
