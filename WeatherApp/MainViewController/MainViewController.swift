c5937bb4252591d2730874541ab156f35db3b444
Defalt Lee <genesis20150601@gmail.com>
2022-04-14 22:16:08 +0800

    Complete

--- WeatherApp/MainViewController/MainViewController.swift
+++ WeatherApp/MainViewController/MainViewController.swift
@@ -11,6 +11,9 @@ class MainViewController: UIViewController {
     
     @IBOutlet weak var divisionLabel: UILabel!
     @IBOutlet weak var weatherLabel: UILabel!
+    @IBOutlet weak var comfortableLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var rainfallLabel: UILabel!
     @IBOutlet weak var checkWeatherBtn: UIButton!
     @IBOutlet weak var divisionPicker: UIPickerView!
     
@@ -22,7 +25,7 @@ class MainViewController: UIViewController {
     let divisions = ["基隆市","新北市","臺北市","桃園市","新竹縣","新竹市","苗栗縣","臺中市","彰化縣","南投縣","雲林縣","嘉義縣","嘉義市","臺南市","高雄市","屏東縣","宜蘭縣","花蓮縣","臺東縣","澎湖縣","金門縣","連江縣"]
     
     // 選擇的行政區
-    var selectedDivision: String?
+    var selectedDivision: String = "基隆市"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,6 +43,7 @@ class MainViewController: UIViewController {
                 self.view.layoutIfNeeded()
             }
         } else {
+            getWeatherData { error in print(error) }
         }
     }
     
@@ -50,6 +54,67 @@ class MainViewController: UIViewController {
     func setDelegate() {
         divisionPicker.delegate = self
         divisionPicker.dataSource = self
+    }
+    
+    func getWeatherData(completion: @escaping((String?) -> Void)) {
+        divisionLabel.text = divisions[self.divisionPicker.selectedRow(inComponent: 0)]
+        
+        var lowTempString: String?
+        var highTempString: String?
+        
+        // 建立 URLRequest
+        var request = URLRequest(url: URL(string: "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-F833BDA4-8A19-4626-9A1D-1806F412FF3C&format=JSON&elementName=&sort=time&startTime=")!,timeoutInterval: Double.infinity)
+        
+        // API 請求方式
+        request.httpMethod = "GET"
+        
+        // API 會自動切到副執行緒
+        let task = URLSession.shared.dataTask(with: request) { [self] (getData, respond, error) in
+            let decoder = JSONDecoder() // 建立 Json 格式的 Decoder
+            if let weatherData = getData, let weather = try? decoder.decode(Weather.self, from: weatherData) {
+                weather.records.location.forEach({ (location) in
+                    guard location.locationName == self.selectedDivision else { return }
+                    print(location.locationName)
+                    location.weatherElement.forEach { (weatherElement) in
+                        let data = weatherElement.time[2].parameter
+                        switch weatherElement.elementName {
+                        case "Wx": print("  天氣現象")
+                            DispatchQueue.main.async {
+                                self.weatherLabel.text = "\(data.parameterName)\(data.parameterUnit ?? "")"
+                            }
+                        case "MaxT": print("  最高溫")
+                            highTempString = "\(data.parameterName) °C"
+                        case "MinT": print("  最低溫")
+                            lowTempString = "\(data.parameterName) °C"
+                        case "CI": print("  舒適度")
+                            DispatchQueue.main.async {
+                                self.comfortableLabel.text = "\(data.parameterName)\(data.parameterUnit ?? "")"
+                            }
+                        case "PoP": print("  降雨機率")
+                            DispatchQueue.main.async {
+                                self.rainfallLabel.text = "\(data.parameterName) %"
+                            }
+                        default: break
+                        }
+                        
+                        /*
+                        weatherElement.time.forEach { (time) in
+                            print("    \(time.startTime) ~ \(time.endTime)")
+                            print("    \(time.parameter.parameterName)\(time.parameter.parameterUnit ?? "")")
+                        }
+                        */
+                    }
+                })
+                
+                DispatchQueue.main.async { [self] in
+                    temperatureLabel.text = "\(lowTempString!) ~ \(highTempString!)"
+                }
+                completion(nil)
+            } else {
+                completion("error")
+            }
+        }
+        task.resume()
     }
     
 }
