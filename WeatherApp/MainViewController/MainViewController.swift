f66032565c5675245ae9a7f24c5aee88b1f4f9d1
Defalt Lee <genesis20150601@gmail.com>
2022-04-16 12:33:13 +0800

    Complete Adjust

--- WeatherApp/MainViewController/MainViewController.swift
+++ WeatherApp/MainViewController/MainViewController.swift
@@ -22,7 +22,13 @@ class MainViewController: UIViewController {
     var safeAreaInset: UIEdgeInsets?
     
     // 行政區
-    let divisions = ["基隆市","新北市","臺北市","桃園市","新竹縣","新竹市","苗栗縣","臺中市","彰化縣","南投縣","雲林縣","嘉義縣","嘉義市","臺南市","高雄市","屏東縣","宜蘭縣","花蓮縣","臺東縣","澎湖縣","金門縣","連江縣"]
+    let divisions = ["基隆市","新北市","臺北市",
+                     "桃園市","新竹縣","新竹市","苗栗縣",
+                     "臺中市","彰化縣","南投縣",
+                     "雲林縣","嘉義縣","嘉義市","臺南市",
+                     "高雄市","屏東縣",
+                     "宜蘭縣","花蓮縣","臺東縣",
+                     "澎湖縣","金門縣","連江縣"]
     
     // 選擇的行政區
     var selectedDivision: String = "基隆市"
@@ -30,7 +36,6 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         safeAreaInset = UIApplication.shared.windows.first?.safeAreaInsets
-        // Do any additional setup after loading the view.
         
         setView()
         setDelegate()
@@ -43,12 +48,18 @@ class MainViewController: UIViewController {
                 self.view.layoutIfNeeded()
             }
         } else {
-            getWeatherData { error in print(error) }
+            getWeatherData { error in print(error as Any) }
         }
     }
     
     func setView() {
         cons_divisionPickerBottom.constant = -divisionPicker.frame.height - safeAreaInset!.bottom
+        
+        divisionLabel.text = "天氣預報"
+        weatherLabel.text = ""
+        comfortableLabel.text = ""
+        temperatureLabel.text = ""
+        rainfallLabel.text = ""
     }
     
     func setDelegate() {
@@ -59,16 +70,16 @@ class MainViewController: UIViewController {
     func getWeatherData(completion: @escaping((String?) -> Void)) {
         divisionLabel.text = divisions[self.divisionPicker.selectedRow(inComponent: 0)]
         
-        var lowTempString: String?
-        var highTempString: String?
+        var Wx:     String = ""
+        var MinT:   String = ""
+        var MaxT:   String = ""
+        var CI:     String = ""
+        var PoP:    String = ""
         
-        // 建立 URLRequest
         var request = URLRequest(url: URL(string: "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-F833BDA4-8A19-4626-9A1D-1806F412FF3C&format=JSON&elementName=&sort=time&startTime=")!,timeoutInterval: Double.infinity)
         
-        // API 請求方式
         request.httpMethod = "GET"
         
-        // API 會自動切到副執行緒
         let task = URLSession.shared.dataTask(with: request) { [self] (getData, respond, error) in
             let decoder = JSONDecoder() // 建立 Json 格式的 Decoder
             if let weatherData = getData, let weather = try? decoder.decode(Weather.self, from: weatherData) {
@@ -77,38 +88,30 @@ class MainViewController: UIViewController {
                     print(location.locationName)
                     location.weatherElement.forEach { (weatherElement) in
                         let data = weatherElement.time[2].parameter
+                        
                         switch weatherElement.elementName {
-                        case "Wx": print("  天氣現象")
-                            DispatchQueue.main.async {
-                                self.weatherLabel.text = "\(data.parameterName)\(data.parameterUnit ?? "")"
-                            }
-                        case "MaxT": print("  最高溫")
-                            highTempString = "\(data.parameterName) °C"
-                        case "MinT": print("  最低溫")
-                            lowTempString = "\(data.parameterName) °C"
-                        case "CI": print("  舒適度")
-                            DispatchQueue.main.async {
-                                self.comfortableLabel.text = "\(data.parameterName)\(data.parameterUnit ?? "")"
-                            }
-                        case "PoP": print("  降雨機率")
-                            DispatchQueue.main.async {
-                                self.rainfallLabel.text = "\(data.parameterName) %"
-                            }
+                        case "Wx":
+                            Wx = "\(data.parameterName)\(data.parameterUnit ?? "")"
+                        case "MaxT":
+                            MaxT = "\(data.parameterName) °C"
+                        case "MinT":
+                            MinT = "\(data.parameterName) °C"
+                        case "CI":
+                            CI = "\(data.parameterName)\(data.parameterUnit ?? "")"
+                        case "PoP":
+                            PoP = "\(data.parameterName) %"
                         default: break
                         }
-                        
-                        /*
-                        weatherElement.time.forEach { (time) in
-                            print("    \(time.startTime) ~ \(time.endTime)")
-                            print("    \(time.parameter.parameterName)\(time.parameter.parameterUnit ?? "")")
-                        }
-                        */
                     }
                 })
                 
                 DispatchQueue.main.async { [self] in
-                    temperatureLabel.text = "\(lowTempString!) ~ \(highTempString!)"
+                    weatherLabel.text = Wx
+                    temperatureLabel.text = "\(MinT) ~ \(MaxT)"
+                    comfortableLabel.text = CI
+                    rainfallLabel.text = PoP
                 }
+                    
                 completion(nil)
             } else {
                 completion("error")
